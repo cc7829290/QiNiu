2c0080ae911012c5219cb2c26f20ff6f307cd8b4
陈超 <759667142@qq.com>
2017-03-12 00:30:06 +0800

    增加库引用 以及界面显示

--- QINIU/MainWindow.xaml.cs
+++ QINIU/MainWindow.xaml.cs
@@ -17,7 +17,7 @@ namespace QINIU
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
