7cca147301da1350bb29a8350072462f59349e85
陈超 <759667142@qq.com>
2017-03-13 00:17:45 +0800

    增加主题引用

--- QINIU/MainWindow.xaml.cs
+++ QINIU/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QINIU.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,14 @@ namespace QINIU
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void GotoSetting(object sender, RoutedEventArgs e)
+        {
+            SettingWindow win = new SettingWindow();
+            win.Owner = this;
+            win.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            win.ShowDialog();
         }
     }
 }
